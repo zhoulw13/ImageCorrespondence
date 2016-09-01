4e2ed2f3d6c66aaa91e3ad9f9a2106828f21cb3b
ivan <zhoulw063922@gmail.com>
2016-09-01 15:47:18 +0800

    is it the end?

--- c++_code/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include "mex.h" /* Always include this */
-
-void mexFunction(int nlhs, mxArray *plhs[], /* Output variables */
-				 int nrhs, const mxArray *prhs[]) /* Input variables */
-{
-	mexPrintf("%d\n", nrhs);
-	mexPrintf("Hello, world!\n"); /* Do something interesting */
-	return;
-}
--- demo.m
+++ demo.m
@@ -14,33 +14,28 @@ if ~exist('pbCGTG')
     addpath('superpixels64\segmentationBerkeley\lib\matlab\');
 end
 
-if true
-    addpath('matlab_code_2');
-end
-
 %Src_path        = 's1\bei_src.png'; % src filename
 %Ref_path        = 's1\bei_ref.png'; % ref filename 
-%Src_path = 'bark.png';
-%Ref_path = 'bark_ref.png';
-Src_path = 'card.png';
-Ref_path = 'card_ref.png';
-
 
 %% initialize operation
 %[Corr, Con, Src, Ref] = nrdc_example(Src_path, Ref_path);
 %[Sp, Seg, Sp2] = sp_demo(Src);
+Src_lab = rgb2lab(Src);
+Ref_lab = rgb2lab(Ref);
+Src_lab(:,:,1) = Src_lab(:,:,1)/100;
+Src_lab(:,:,2) = (Src_lab(:,:,2)+128)/256;
+Src_lab(:,:,3) = (Src_lab(:,:,3)+128)/256;
 
-
-labTransformation = makecform('srgb2lab');
-Src_lab = applycform(Src,labTransformation);
-Ref_lab = applycform(Ref,labTransformation);
+Ref_lab(:,:,1) = Ref_lab(:,:,1)/100;
+Ref_lab(:,:,2) = (Ref_lab(:,:,2)+128)/256;
+Ref_lab(:,:,3) = (Ref_lab(:,:,3)+128)/256;
 
 %[sp_set, sp_val, bspline_set, ref_set] = segmentation(Sp2, Con, Corr, Ref);
 %[sp_set, bspline_set, ref_set, adjacency_graph] = merging(sp_set, sp_val, bspline_set, ref_set, Sp2, Ref_lab);
-%[errors, sp_set, ref_set, bspline_set, newCorr] = extension(sp_set, bspline_set, Sp2, Src_lab, Ref_lab);
+%[errors, sp_set, ref_set, bspline_set, newCorr] = extension(sp_set, ref_set, bspline_set, Sp2, Src_lab, Ref_lab);
 
-%[sp_set, sp_val, bspline_set, ref_set] = segmentation_mex(Sp2, Confidence, DCF);
-%[sp_set, bspline_set, ref_set, adjacency_graph] = merging_mex(sp_set, sp_val, bspline_set, ref_set, Sp2, Ref_lab);
-[errors, sp_set, ref_set, bspline_set, newCorr] = extension_mex(sp_set, bspline_set, Sp2, Src_lab, Ref_lab);
+[sp_set, sp_val, bspline_set, ref_set] = Correspondence('segmentation', Sp2, double(Con), Corr, Ref);
+[sp_set, bspline_set, ref_set, adjacency_graph] = Correspondence('merging', sp_set, sp_val, bspline_set, ref_set, Sp2, Ref_lab);
+[errors, sp_set, ref_set, bspline_set, newCorr] = Correspondence('extension', sp_set, ref_set, bspline_set, Sp2, Src_lab, Ref_lab);
 
 
